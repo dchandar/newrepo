62bb06e29e53cb937f3587a05e0ab95f96d516d2
Dhruv Chandarana <chandaranadhruv24@gmail.com>
2017-02-22 03:33:56 +0000

    First

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'store#index'
+
+root to: 'store#index', as: 'store'
 
 end
